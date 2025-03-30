095f5495fbcfc3f4044d56797e32d5b358cfd3b4
Fernando Saucedo <contactofernandosaucedo@gmail.com>
2025-03-30 20:25:59 -0300

    Selecionado elementos de la interfaz

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Esperar que se cargue todo el documento HTML ...
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Seleccionar los elementos de la interfaz
+    const inputEmail = document.querySelector('#email');
+    const inputEmail = document.querySelector('#asunto');
+    
+});
