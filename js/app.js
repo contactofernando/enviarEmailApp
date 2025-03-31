eed3175b71ca4c231460c4467c2381837a25e048
Fernando Saucedo <contactofernandosaucedo@gmail.com>
2025-03-31 00:32:48 -0300

    Eventos para validar formularios

--- js/app.js
+++ js/app.js
@@ -3,6 +3,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Seleccionar los elementos de la interfaz
     const inputEmail = document.querySelector('#email');
-    const inputEmail = document.querySelector('#asunto');
+    const inputAsunto = document.querySelector('#asunto');
+    const inputMensaje = document.querySelector('#mensaje');
     
+    // Asignar eventos
+    inputEmail.addEventListener('blur', function(e) {
+        
+    });
 });
