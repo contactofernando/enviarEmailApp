0c576a25ff9cdc2eba47c4cf6f42af0e6b3916a1
Fernando Saucedo <contactofernandosaucedo@gmail.com>
2025-04-23 12:42:06 -0300

    Agregado spinner cuando se envia email y reseteo de formulario

--- js/app.js
+++ js/app.js
@@ -25,14 +25,7 @@ document.addEventListener('DOMContentLoaded', function() {
     
     btnReset.addEventListener('click', function(e) {
         e.preventDefault();
-
-        // Reiniciar el objeto
-        email.email = '';
-        email.asunto = '';
-        email.mensaje = '';
-
-        formulario.reset();
-        comprobarEmail();
+        resetFormulario();
     })
 
     function enviarEmail(e) {
@@ -40,6 +33,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
         spinner.classList.add('flex');
         spinner.classList.remove('hidden');
+
+        setTimeout(() => {
+            spinner.classList.remove('flex');
+            spinner.classList.add('hidden');
+
+            resetFormulario();
+        }, 3000);
     }
 
     function validar(e) {
@@ -103,5 +103,15 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         btnSubmit.classList.remove('opacity-50');
         btnSubmit.disabled = false; 
+    }
+
+    function resetFormulario() {
+                // Reiniciar el objeto
+                email.email = '';
+                email.asunto = '';
+                email.mensaje = '';
+        
+                formulario.reset();
+                comprobarEmail();
     }
 });
